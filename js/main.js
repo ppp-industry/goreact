54adf5ae9aa21c7c18badfa5b83be5336859bad7
zed-web-m <72280188+zed-web-m@users.noreply.github.com>
2022-09-06 20:35:56 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -1,16 +1,53 @@
+const burger = document.querySelector('.menu-btn');
+const header = document.querySelector('.header');
+
+burger.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    burger.classList.toggle('active');
+
+    header.classList.toggle('open');
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+    if (header.classList.contains('open')) {
+        document.querySelector('body').style.overflowY = 'hidden';
+    } else {
+        document.querySelector('body').style.overflowY = 'auto';
+    }
+})
+
+const overlay = document.querySelector('.overlay');
+
+overlay.addEventListener('click', () => {
+    header.classList.remove('open');
+    burger.classList.remove('active');
+});
+
 const swiperBenefits = new Swiper ('.benefits-slider', {
     speed: 800,
     slidesPerView: 2,
     loop: true,
     autoplay: true,
-    spaceBetween: 32,
     navigation: {
         nextEl: '.benefits-next',
         prevEl: '.benefits-prev',
     },
     breakpoints: {
-        686: {
-
+        320: {
+            spaceBetween: 17,
+            slidesPerView: 1.1,
+        },
+        481: {
+            spaceBetween: 32,
+            slidesPerView: 1.5,
+        },
+        769: {
+            spaceBetween: 32,
+            slidesPerView: 2,
         },
     }
 })
